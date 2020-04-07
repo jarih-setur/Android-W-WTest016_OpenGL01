8602e9f3229caf45bb3474dfe5619c5a79a4910f
Jari í Hjøllum <jarih@setur.fo>
2020-04-07 22:58:13 +0100

    update

--- app/src/main/java/se/miun/jarhjo/dt031g/wtest016_opengltest01/MyGLRenderer.java
+++ app/src/main/java/se/miun/jarhjo/dt031g/wtest016_opengltest01/MyGLRenderer.java
@@ -4,6 +4,7 @@ import javax.microedition.khronos.opengles.GL10;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 
 public class MyGLRenderer implements GLSurfaceView.Renderer {
 
@@ -34,13 +35,41 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         mSquare = new Square();
     }
 
+//    public void onDrawFrame(GL10 unused) {
+//        // Redraw background color
+//        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+//        mTriangle.draw();
+//    }
+
+    @Override
     public void onDrawFrame(GL10 unused) {
-        // Redraw background color
+                // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-        mTriangle.draw();
+        // Set the camera position (View matrix)
+        Matrix.setLookAtM(viewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+
+        // Draw shape
+        mTriangle.draw(vPMatrix);
     }
 
+
+    // vPMatrix is an abbreviation for "Model View Projection Matrix"
+    private final float[] vPMatrix = new float[16];
+    private final float[] projectionMatrix = new float[16];
+    private final float[] viewMatrix = new float[16];
+
+    @Override
     public void onSurfaceChanged(GL10 unused, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
+
+        float ratio = (float) width / height;
+
+        // this projection matrix is applied to object coordinates
+        // in the onDrawFrame() method
+        Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
     }
+
 }
